37933b91942eef427078b1270284d6bf50973c74
maomaoChang <32168003+maomaoChang@users.noreply.github.com>
2018-06-03 13:27:58 +0800

    update EightQueen algorithm

--- Classical algorithm/EightQueen.java
+++ Classical algorithm/EightQueen.java
@@ -9,7 +9,7 @@
      那么就得到了其中一种正确的解法。
    2.定义二位数组A[8][8],int型。0表示没有摆放皇后，1表示已经摆放皇后
    3.如何检查拟摆放皇后的位置(x,y)是否符合要求：
-     a:纵向检查, (x,y-t)的值不为1
+     a:纵向检查, (x-t,y)的值不为1
      b:左斜上方, (x-t,y-t)的值不为1
      c:由斜上方, (x+t,y-t)的值不为1
    4.
@@ -17,8 +17,9 @@
 
 public class EightQueen{
 
- 	private static final int MAX_QUEEN = 8;
- 	private int[][] chessBoard;
+ 	private static final int MAX_QUEEN = 8; //后期适度扩展至多皇后(>8)问题，并测试该写法的性能
+ 	private static int[][] chessBoard;
+ 	private static int count=0;            //结果计数器
 
  	public EightQueen(){
  		this.chessBoard = new int[MAX_QUEEN][MAX_QUEEN];
@@ -27,63 +28,80 @@ public class EightQueen{
 
 
  	public static void main(String[] args) {
- 		System.out.println("hello world!");
-
-
+ 		//对象初始化
  		EightQueen queen = new EightQueen();
- 		queen.setLocation2(0); //从第1个皇后开始
+
+ 		queen.setLocation(0); //从第1个皇后开始
+ 		/* 
+ 		 //调试checkLocation方法
+ 		chessBoard[0][0] = 1;
+ 		chessBoard[1][2] = 1;
+ 		chessBoard[2][4] = 1;
+ 		chessBoard[3][1] = 1;
+ 		chessBoard[4][3] = 1;
+ 		int t = 5;
+ 		for (int m = 0; m < MAX_QUEEN; m++){
+ 			System.out.println(t+","+m+" : "+queen.checkLocation(t,m));	
+ 		} 		
+	    */
  		queen.printSolution();
 
- 		//queen.putQueen();
+
+ 		
  	}
 
- 	//检查当前位置能否摆放皇后,true:可以放，false:不能放
- 	public boolean checkLocation(int x, int y){
+    //摆放皇后
+ 	public boolean setLocation(int x){
 
- 		for(int i = 0; i < y; i++){
-            //纵向
- 			if(chessBoard[x][i] == 1){
- 				return false;
- 			}
- 			//左斜上方
- 			if(x-1-i >= 0 && chessBoard[x-1-i][y-1-i] == 1){
- 				return false;
- 			}
- 			//右斜上方
- 			if(x+1+i < MAX_QUEEN && chessBoard[x+1+i][y-1-i] == 1){
- 				return false;
- 			}
- 		}
- 		return true; 
- 	}
-
- 	//摆放
- 	public boolean setLocation(int y){
- 		if(y == MAX_QUEEN){
+ 		if(x == MAX_QUEEN){
  			return true; //最后一个皇后摆放完毕
  		}
 
  		for(int i = 0; i < MAX_QUEEN; i++){
- 			for(int x=0; x < MAX_QUEEN; x++){
- 				chessBoard[x][y] = 0;
+ 			//
+ 			for(int j=0;  j < MAX_QUEEN; j++){
+ 				chessBoard[x][j] = 0; //清空之前该行的摆放结果，并重新计算
  			}
 
- 			if(checkLocation(i,y)){
- 				chessBoard[i][y]=1;
- 				if(setLocation(y+1)){
- 					return true;
+ 			if(checkLocation(x,i)){
+
+ 				chessBoard[x][i]=1;
+ 				//(x,i)位置已符合要求，置为1，并开始安排下一行，即x+1
+ 				if(setLocation(x+1)){
+ 					return true; 	
  				}
+ 			} 			
+ 		}
+
+ 		return false; //返回false,表示该行无论如何摆放都无法满足要求,回溯到上一层,重新评估月上一层的(x,i+1)位置
+ 	} 
+
+ 	//检查当前位置能否摆放皇后,true:可以放，false:不能放
+ 	public boolean checkLocation(int x, int y){
+
+ 		for(int i = 1; i <= x; i++){
+            //纵向
+ 			if(x-i >= 0 && chessBoard[x-i][y] == 1){
+ 				return false;
+ 			}
+ 			//左斜上方
+ 			if(y-i >= 0 && chessBoard[x-i][y-i] == 1){
+ 				return false;
+ 			}
+ 			//右斜上方
+ 			if(y+i < MAX_QUEEN && chessBoard[x-i][y+i] == 1){
+ 				return false;
  			}
  		}
- 		return false;
- 	}
+ 		return true; 
+ 	} 	
+
 
  	//打印摆放方案
  	public void printSolution(){
  		for(int i = 0; i < MAX_QUEEN; i++){
- 			//第i行
  			for (int j = 0; j < MAX_QUEEN ; j++) {
- 				System.out.print(chessBoard[i][j]);
+ 				System.out.print(chessBoard[i][j] + " ");
  			}
 
  			System.out.println();
