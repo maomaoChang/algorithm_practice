b600ab12a30ca23f6d7796b1593792e37452f1d6
maomaoChang <32168003+maomaoChang@users.noreply.github.com>
2018-06-07 00:12:49 +0800

    optimize binary search

--- Classical algorithm/SearchTest.java
+++ Classical algorithm/SearchTest.java
@@ -1,9 +1,10 @@
 public class SearchTest{
 	public static void main(String[] args) {
 		int[] array = {1,2,3,4,5,6,8,9,10};
+		int[] array2 = {1,2,3,4,5,6,7,8,9,10};
 		int k = 7;
 
-		System.out.println("k的位置在： " + SearchUtil.BinarySearch(array,k) + "(-1表示在数组中未找到该值)" );
+		System.out.println("k的位置在： " + SearchUtil.BinarySearch2(array2,k) + "(-1表示在数组中未找到该值)" );
 	}
 }
 
@@ -28,5 +29,25 @@ class SearchUtil{
 			mid = (min + max ) / 2;
 		}
 		return mid; //返回k在数组中的位置
+	}
+
+	//二分查找的实现方式二
+	static int BinarySearch2(int[] arr, int k){
+		int min=0, max = arr.length-1,mid;
+		mid = (min + max) / 2;
+
+		while(min <= max){
+			mid = (min + max) >> 1; //位运算比较快
+			if( k > arr[mid]){
+				min = mid + 1;
+			}
+			else if( k < arr[mid]){
+				max = mid - 1;
+			}
+			else 
+				return mid; //找到k的位置并返回 //位运算比较快
+		}
+
+		return -1; //未找到
 	}
 }
